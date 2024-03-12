12856e594a5685a3fc13e2ed4225c722cbef6558
Jan Kiesewetter <jan@t3easy.de>
2024-03-12 13:27:58 +0100

    Update UMD header

--- slick/slick.js
+++ slick/slick.js
@@ -16,28 +16,12 @@
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
 ;(function(factory) {
-    'use strict';
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['jquery'], factory);
-    } else if (typeof module === 'object' && module.exports) {
+    } else if (typeof exports === 'object') {
         // Node/CommonJS
-        module.exports = function(root, jQuery) {
-            if (jQuery === undefined) {
-                // require('jQuery') returns a factory that requires window to
-                // build a jQuery instance, we normalize how we use modules
-                // that require this pattern but the window provided is a noop
-                // if it's defined (how jquery works)
-                if (typeof window !== 'undefined') {
-                    jQuery = require('jquery');
-                }
-                else {
-                    jQuery = require('jquery')(root);
-                }
-            }
-            factory(jQuery);
-            return jQuery;
-        };
+        factory(require('jquery'));
     } else {
         // Browser globals
         factory(jQuery);
